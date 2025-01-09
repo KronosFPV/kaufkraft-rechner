36d2d01bdabfed8a5a4d02dba0a1d63b7b042532
KronosFPV <137655816+KronosFPV@users.noreply.github.com>
2025-01-09 17:47:56 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import KaufkraftRechner from '@/components/KaufkraftRechner'
+
+export default function Home() {
+  return (
+    <main className="container mx-auto p-4">
+      <KaufkraftRechner />
+    </main>
+  )
+}
